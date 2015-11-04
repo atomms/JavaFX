f94dd511575452a0844168618358852bf2044776
rippercro <itusek.os@gmail.com>
2015-11-04 22:20:59 +0100

    JavaFX

---
+++ StrokeAndFill/src/StrokeFillEx.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class StrokeFillEx extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        initUI(stage);
+    }
+
+    private void initUI(Stage stage) {
+
+        Pane root = new Pane();
+        
+        Canvas canvas = new Canvas(300, 300);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        doDrawing(gc);
+        
+        root.getChildren().add(canvas);    
+
+        Scene scene = new Scene(root, 300, 250, Color.WHITESMOKE);
+
+        stage.setTitle("Stroke and fill");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    private void doDrawing(GraphicsContext gc) {
+        
+        gc.setStroke(Color.FORESTGREEN.brighter());
+        gc.setLineWidth(5);
+        gc.strokeOval(30, 30, 80, 80);        
+        gc.setFill(Color.FORESTGREEN);
+        gc.fillOval(130, 30, 80, 80);
+    }    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
